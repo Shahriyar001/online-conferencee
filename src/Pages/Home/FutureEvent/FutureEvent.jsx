dbe49dcf8b3c4058e511d26a97331b4e56f14ded
Shahriyar001 <shahriyarmahbub55@gmail.com>
2025-04-19 23:45:54 +0600

    added new componant

---
+++ src/Pages/Home/FutureEvent/FutureEvent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FutureEvent = () => {
+  return (
+    <div className="max-w-[]">
+      <div className="max-w-[1320px] mt-32 bg-gray-300">
+        <h1 className="text-4xl">Future Event</h1>
+      </div>
+    </div>
+  );
+};
+
+export default FutureEvent;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../Banner/Banner";
 import Marquee from "../Marque/Marquee";
+import FutureEvent from "../FutureEvent/FutureEvent";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Marquee />
+      <FutureEvent />
     </div>
   );
 };
